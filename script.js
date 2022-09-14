f0b0b4eaa9316be7840fdbba1794d1517312ba7d
Richard <rten04@icloud.com>
2022-09-14 01:03:08 -0400

    Add functions and constant vars
    Created foundation for future code. Set constant variables for the
    three choices that the player has (rps). Added event listeners to the three
    buttons (rps). Added simple functions to be called upon
    later on (unfinished).

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-const playerSelections = document.querySelectorAll(".playerSelection");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-function playRound(e) {
-    console.log("test");
+function changeScissors() {
+    return playRound()
 };
 
-playerSelections.forEach(playerSelection => playerSelection.addEventListener('click', playRound));
+function changeRock() {
+    return playRound()
+}
+
+function changePaper() {
+    return playRound()
+}
+
+function playRound (choice) {
+
+}
+
+function playerWin () {
+    console.log("You won!");
+}
+
+function compWin () {
+    console.log("You lost!");
+}
+
+function draw () {
+    console.log("You drew!");
+}
+
+scissors.addEventListener('click', changeScissors);
+rock.addEventListener('click', changeRock);
+paper.addEventListener('click', changePaper);
