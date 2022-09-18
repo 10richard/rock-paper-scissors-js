d6572b60c8334ad4256f4e557948649a3d8f9e25
Richard <rten04@icloud.com>
2022-09-17 22:19:13 -0400

    Add/modify functions
    Modified changeScissors, changeRock and changePaper functions to set functions.
    Those functions essentially create a variable with a string corresponding to the player's choice.
    
    Added getCompChoice function. Gives a random choice to the computer by selecting a random number from 0 to 2 (ranChoose)
    then picking out one of the selections from the choices array. ie/ choices[ranChoose] = choices[0, 1 or 2]
    
    Deleted playRound function. Replaced with checkPlayerSelection function, which will check what the player chose, then calls for another function
    to check for a winner through a loop.

--- script.js
+++ script.js
@@ -2,34 +2,58 @@ const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 
-function changeScissors() {
-    return playRound()
+function setScissors() {
+    const playerChoice = "scissors";
+    return getCompChoice(playerChoice);
 };
 
-function changeRock() {
-    return playRound()
+function setRock() {
+    const playerChoice = "rock";
+    return getCompChoice(playerChoice);
 }
 
-function changePaper() {
-    return playRound()
+function setPaper() {
+    const playerChoice = "paper";
+    return getCompChoice(playerChoice);
 }
 
-function playRound (choice) {
-
+function getCompChoice (playerChoice) {
+    const choices = ["rock", "paper", "scissors"];
+    let ranChoose = Math.floor(Math.random() * 3);
+    let compChoice = choices[ranChoose];
+    checkPlayerSelection(playerChoice, compChoice);
 }
 
-function playerWin () {
+function checkPlayerSelection(playerChoice, compChoice) {
+    if (playerChoice === "rock") {
+
+    }
+}
+
+function playerRock(compChoice) {
+    for (let i = 0; i < 5; i++) {
+        if (compChoice === "rock") {
+            tie();
+        } else if (compChoice === "paper") {
+            compWin();
+        } else if (compChoice === "scissors") {
+            playerWin();
+        }
+    }
+}
+
+function playerWin() {
     console.log("You won!");
 }
 
-function compWin () {
+function compWin() {
     console.log("You lost!");
 }
 
-function draw () {
+function tie() {
     console.log("You drew!");
 }
 
-scissors.addEventListener('click', changeScissors);
-rock.addEventListener('click', changeRock);
-paper.addEventListener('click', changePaper);
+scissors.addEventListener('click', setScissors);
+rock.addEventListener('click', setRock);
+paper.addEventListener('click', setPaper);
